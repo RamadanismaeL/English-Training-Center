c1935a0cd28d493f36e3c7484dcef65041e62bf1
RamadanismaeL <ramadan.ismael02@gmail.com>
2024-12-08 00:55:45 +0200

    first commit

---
+++ englishtrainingcenter/src/main/java/com/ram/englishtrainingcenter/HelloController.java
@@ -0,0 +1,14 @@
+package com.ram.englishtrainingcenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
